dc351ecac0f6aeecfc12b2062f8d403cc57fc526
anuoluwapo <anuoluwapokudirat@yahoo.com>
2020-08-20 16:10:44 +0000

    created 16X16 grids

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let container = document.getElementById('container');
+// const square = document.createElement('div');
+// function squares(){
+//     for(let i=0; i<=16; i++){
+//         const square = document.createElement('div')
+//         container.appendChild(square);
+//     }
+//     return container;
+// }
+
+// squares();
+for(let i=0; i<=16; i++){
+    const squares = document.createElement('div')
+    container.appendChild(squares);
+}
